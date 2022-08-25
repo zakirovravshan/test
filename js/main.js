fd81645c496bd98f698bfe80cd70c310c23852b7
zakirovravshan <zakirovrovshan@gmail.com>
2022-08-25 19:03:12 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var firstName = prompt('Ismingizni kiriting')
+var lastName = prompt('Familiyangizni  kiriting')
+console.log('Salom ' + firstName + ' ' + lastName);
+
+var test1 = prompt('Qatorni sindiruchchi html elemetini belgilang : a)<break> b)<hr> c)<br>' )
+console.log('Birinchi test ! Sizning javobingiz ' + test1 + ' ' + 'Togri Javob : c')
+
+var test2 = prompt('Tartibli list ichiga yozib bolmaydi : a) <h1> b) <ul> c) a+b' )
+console.log('Ikkinchi test ! Sizning javobingiz ' + test2 + ' ' + 'Togri Javob : c')
+
+var test3 = prompt('HTML bu  : a) Yaratish tili b) Stillash tili  c) Belgilash tili ' )
+console.log('Uchinchi test ! Sizning javobingiz ' + test3 + ' ' + 'Togri Javob : c')
+
+var test4 = prompt('DOKA <h2> ni necha marotaba ishlatishni maslaxat bergan : a) 3 b) 2 c) 4tagacha' )
+console.log('Tortinchi test ! Sizning javobingiz ' + test4 + ' ' + 'Togri Javob : b')
+
+var test5 = prompt('<strong> tegi bu qanday element: a) block b) inline c) inline-block' )
+console.log('Beshinchi test ! Sizning javobingiz ' + test5 + ' ' + 'Togri Javob : b')
+
+var test6 = prompt('Textni kursiv qiyshiq yozish uchun teg : a) <u> b) <em> c) <rem>' )
+console.log('Oltinchi test ! Sizning javobingiz ' + test6 + ' ' + 'Togri Javob : b')
+
+var test7 = prompt('Font weight 600 nomlanishi qanday ?: a) extrabold b) semi-bold c) light ' )
+console.log('Yettinchi test ! Sizning javobingiz ' + test7 + ' ' + 'Togri Javob : b')
+
+var test8 = prompt('<a> tegining zarur atributi  : a) <link> b) <traget_blank> c) href' )
+console.log('Sakkizinchi test ! Sizning javobingiz ' + test8 + ' ' + 'Togri Javob : c')
+
+var test9 = prompt('Qizil rangning HEX kodi : a)#FF0000  b)#0000FF c)#FF0011' )
+console.log('Toqqizinchi test ! Sizning javobingiz ' + test9 + ' ' + 'Togri Javob : a')
+
+var test10 = prompt('Padding box model da qanday rang bilan korsatiladi : a) kok b) yashil c) siyoh' )
+console.log('Oninchi test ! Sizning javobingiz ' + test10 + ' ' + 'Togri Javob : b')
